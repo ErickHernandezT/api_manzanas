79acf15f651608fbae58a3a311acd481d62dcec6
ErickHernandezTo <L19TE0561@teziutlan.tecnm.mx>
2023-09-27 09:28:56 -0600

    Correcion de imagen manzana

--- src/Application/Actions/Manzana/manzanaController.php
+++ src/Application/Actions/Manzana/manzanaController.php
@@ -19,26 +19,23 @@ class manzanaController extends generalController
     public function validarIngresarManzanas($request, $response, $args)
     {
         $params = (array)$request->getParsedBody();
-
+    
         $nombre = (isset($params['nombre'])) ? strip_tags($params['nombre']) : '';
         $nivelMadurez = (isset($params['nivelMadurez'])) ? strip_tags($params['nivelMadurez']) : '';
         $descripcion = (isset($params['descripcion'])) ? strip_tags($params['descripcion']) : '';
         $estatus = (isset($params['estatus'])) ? (int)strip_tags($params['estatus']) : 0;
         $precio = (isset($params['precio'])) ? (float)strip_tags($params['precio']) : 0.0;
         $stock = (isset($params['stock'])) ? (int)strip_tags($params['stock']) : 0;
-
-        $uploadedFile = $request->getUploadedFiles()['foto'];
-
-        $mensaje = ['message' => ''];
-
-        if ($nombre != '' && $nivelMadurez != '' && $descripcion != '' && $estatus > 0 && $precio > 0 && $stock > 0 && $descripcion != '') {
-            // Verifica que la foto se haya cargado correctamente
-            if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
-                // El archivo se cargó correctamente
-                $blobData = $uploadedFile->getStream();
-
+        $base64Data = (isset($params['foto'])) ? $params['foto'] : '';
+    
+        if ($nombre != '' && $nivelMadurez != '' && $descripcion != '' && $estatus > 0 && $precio > 0 && $stock > 0 && $descripcion != '' && $base64Data != '') {
+            // Decodificamos la cadena Base64
+            $blobData = base64_decode($base64Data);
+    
+            if ($blobData !== false) {
+                // Verifica que la foto se haya cargado correctamente
                 $mensaje = $this->funciones->ingresarManzanas($nombre, $blobData, $nivelMadurez, $descripcion, $estatus, $precio, $stock);
-
+    
                 if ($mensaje) {
                     $code = 200;
                 } else {
@@ -46,17 +43,17 @@ class manzanaController extends generalController
                 }
             } else {
                 $code = 400;
-                $mensaje = ['message' => 'Error al cargar la foto'];
+                $mensaje = ['message' => 'La cadena Base64 no pudo ser decodificada correctamente'];
             }
         } else {
             $code = 400;
             $mensaje = ['message' => 'Datos incorrectos o vacíos'];
         }
-
+    
         // Retornamos la respuesta
         return $this->response($code, [$mensaje], $response);
     }
-
+    
 
 
     public function validarListaManzanas($request, $response, $args)
